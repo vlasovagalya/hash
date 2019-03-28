278631fb74ef5c47aa41884f8945cfdee2e608b4
vlasovagalya <48350627+vlasovagalya@users.noreply.github.com>
2019-03-28 23:32:05 +0500

    Add files via upload

---
+++ Hash.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+
+{
+    class Hash
+    {
+        class KeyValue
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+        List<List<KeyValue>> list;
+
+        class HashData
+        {
+            public readonly int KeyHash;
+            public object Value { get; set; }
+
+            public HashData(int h, object obj)
+            {
+                KeyHash = h;
+                Value = obj;
+            }
+        }
+        private List<HashData>[] data;
+        
+        public Hash(int size)
+        {
+            data = new List<HashData>[size];
+        }
+        
+        public void PutPair(object key, object value)
+        {
+            var index = Math.Abs(key.GetHashCode()) & list.Count;
+            var keyHashCode = key.GetHashCode();
+            foreach (var p in list[index])
+            {
+                if (Equals(p.Key, key))
+                {
+                    p.Value = value;
+                    return;
+                }
+            }
+
+            list[index].Add(new KeyValue { Key = key, Value = value });
+        }
+        public object GetValueByKey(object key)
+        {
+            var BucketNo = Math.Abs(key.GetHashCode()) & list.Count;
+            foreach (var p in list[BucketNo])
+            {
+                if (Equals(p.Key, key))
+                {
+                    return p.Value;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ HashTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using NUnit.Framework;
+      
+        
+
+namespace HashTests
+{
+    [TestFixture]
+    public class HashTests
+    {
+        
+        [Test]
+        public void AddingThreeElementsTest()
+        {
+            var key1 = "int";
+            var key2 = 4;
+            var key3 = 3;
+            var ht = new HashTable.Hash(3);
+            ht.PutPair(key1, 2);
+            ht.PutPair(key2, 34);
+            ht.PutPair(key3, new[] { 0, 1, 2 });
+            Assert.AreEqual(ht.GetValueByKey(key1), 2);
+            Assert.AreEqual(ht.GetValueByKey(key2), 34);
+            Assert.AreEqual(ht.GetValueByKey(key3), new[] { 0, 1, 2 });
+        }
+
+        [Test]
+        public void TwoEquialsElementsTest()
+        {
+            var element = "el2";
+            var newElement = "new";
+            var ht = new HashTable.Hash(3);
+            ht.PutPair(2, element);
+            ht.PutPair(2, newElement);
+            Assert.AreEqual(newElement, ht.GetValueByKey(2));
+        }
+
+        [Test]
+        public void ThousandsElementsTest()
+        {
+        
+            var size = 10000;
+            var ht = new HashTable.Hash(size);
+            for (int i = 0; i < size; i++)
+               {
+                   ht.PutPair(i, i + 1);
+               }
+            Assert.AreEqual(ht.GetValueByKey(15), 16);
+            
+        }
+        [Test]
+        public void FindingAlienKeys()
+        {
+            var size = 10000;
+            var ht = new HashTable.Hash(size);
+
+            for (int i = 1; i < size; i++)
+            {
+                ht.PutPair(i, i + 1);
+            }
+
+            for (int i = size; i < 11000; i++)
+            {
+                Assert.AreEqual(ht.GetValueByKey(i), null);
+            }
+        }
+    }
+}
